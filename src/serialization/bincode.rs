83f47cf8254d6aceaf60c6e2b2598b9847b5c448
Timon Post <timonpost@hotmail.nl>
2020-02-29 23:49:28 +0100

    Removed .description() call.

--- src/serialization/bincode.rs
+++ src/serialization/bincode.rs
@@ -10,18 +10,18 @@ pub struct Bincode;
 impl SerializationStrategy for Bincode {
     fn serialize<I: Serialize>(&self, input: &I) -> Result<Vec<u8>, ErrorKind> {
         Ok(bincode::serialize(&input)
-            .map_err(|e| ErrorKind::SerializationError(e.description().to_string()))?)
+            .map_err(|e| ErrorKind::SerializationError(e.to_string()))?)
     }
 
     fn deserialize<'a, T: Deserialize<'a>>(&self, buffer: &'a [u8]) -> Result<T, ErrorKind> {
         Ok(bincode::deserialize::<T>(buffer)
-            .map_err(|e| ErrorKind::SerializationError(e.description().to_string()))?)
+            .map_err(|e| ErrorKind::SerializationError(e.to_string()))?)
     }
 
     fn apply_to<C: SerdeDiff>(&self, component: &mut C, data: &[u8]) -> Result<(), ErrorKind> {
         bincode::config()
             .deserialize_seed(serde_diff::Apply::deserializable(component), data)
-            .map_err(|e| ErrorKind::SerializationError(e.description().to_string()))?;
+            .map_err(|e| ErrorKind::SerializationError(e.to_string()))?;
 
         Ok(())
     }
